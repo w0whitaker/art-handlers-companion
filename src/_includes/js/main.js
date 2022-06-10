61995a3da46590c5afef1e984d973f472404b34a
William Whitaker <billydwhitaker@gmail.com>
2022-06-10 13:37:14 -0400

    set up pushing widths to array

--- src/_includes/js/main.js
+++ src/_includes/js/main.js
@@ -1,1 +1,24 @@
-console.log('ohai');
+const ohai = function () {
+  console.log('ohai!');
+};
+
+const formSubmit = function (event) {
+  pictureWidths.push(form.add_picture.value);
+  console.log(pictureWidths);
+  addToPictureList(form.add_picture.value);
+  form.reset();
+  event.preventDefault();
+};
+
+function addToPictureList(value) {
+  const newListItem = document.createElement('li');
+  const newListItemContent = document.createTextNode(value);
+  newListItem.appendChild(newListItemContent);
+  output.append(newListItem);
+}
+
+const form = document.getElementById('form');
+const output = document.getElementById('output');
+form.addEventListener('submit', formSubmit);
+
+const pictureWidths = [];
