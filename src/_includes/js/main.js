20e5ccd4819105196227c4d20154a291990cb24b
William Whitaker <billydwhitaker@gmail.com>
2022-06-11 13:46:36 -0400

    wip--display of widths

--- src/_includes/js/main.js
+++ src/_includes/js/main.js
@@ -1,14 +1,13 @@
-const ohai = function () {
-  console.log('ohai!');
-};
+const pictureWidths = [];
 
-const formSubmit = function (event) {
-  pictureWidths.push(form.add_picture.value);
-  console.log(pictureWidths);
-  addToPictureList(form.add_picture.value);
+function formSubmit(event) {
+  const val = Number.parseInt(form.add_picture.value);
+  pictureWidths.push(val);
+  addToPictureList(val);
+  displayTotal(sumOfPictureWidths(pictureWidths));
   form.reset();
   event.preventDefault();
-};
+}
 
 function addToPictureList(value) {
   const newListItem = document.createElement('li');
@@ -17,8 +16,19 @@ function addToPictureList(value) {
   output.append(newListItem);
 }
 
+function sumOfPictureWidths(arr) {
+  const initialValue = 0;
+  return arr.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+}
+
+function displayTotal(sum) {
+  total.textContent = `total width: ${sum}`;
+}
+
 const form = document.getElementById('form');
 const output = document.getElementById('output');
+const total = document.getElementById('total');
 form.addEventListener('submit', formSubmit);
-
-const pictureWidths = [];
