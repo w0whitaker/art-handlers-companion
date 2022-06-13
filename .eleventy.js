587da20335c00d7e2c89f9847ac09af15c830656
William Whitaker <billydwhitaker@gmail.com>
2022-06-13 19:58:00 -0400

    finishes input forms

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,21 @@
 const classNames = require('classnames');
 
-const section = (content, className, title) => {
+const section = (content, cssClass, title) => {
   return `<section class=${classNames(
-    className,
+    cssClass,
     'content'
   )}><h3>${title}</h3><div class="container">${content}</div></section>`;
+};
+
+const forms = (formType) => {
+  return `<form id="${formType}-form"><h4>${formType}</h4><ul><li><label for="input_field">value:</label><input type="text" id="${formType}" name="enter_${formType}"></li><li><button type="submit">submit</button></li></ul></form>`;
 };
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/style.css');
   eleventyConfig.addPassthroughCopy('src/main.js');
   eleventyConfig.addPairedShortcode('section', section);
+  eleventyConfig.addShortcode('form', forms);
 
   // Set custom directories for input, output, includes, and data
   return {
--- src/_includes/js/main.js
+++ src/_includes/js/main.js
@@ -1,34 +1,28 @@
 const pictureWidths = [];
 
-function formSubmit(event) {
-  addToPictureWidthsArray(form.add_picture.value);
-  addWallWidth(form.add_wall.value);
-  form.reset();
+function pictureFormSubmit(event) {
+  pictureHandler(pictureForm.enter_picture.value);
+  pictureForm.reset();
   event.preventDefault();
 }
 
-function addWallWidth(val) {
-  displayWallWidth(Number.parseInt(val));
-}
-
-function displayWallWidth(val) {
-  totalWall.textContent = `wall width ${val}`;
-}
-
-function addToPictureWidthsArray(val) {
+function pictureHandler(val) {
+  console.log(`val @ pictureHandler: ${val}`);
   pictureWidths.push(Number.parseInt(val));
   addToPictureList(Number.parseInt(val));
-  displayTotalWidthOfPictures(sumOfPictureWidths(pictureWidths));
+  console.log(`sumOfArray: ${sumOfArray(pictureWidths)}`);
+  displayTotalWidthOfPictures(sumOfArray(pictureWidths));
 }
 
 function addToPictureList(val) {
+  console.log(`val @ addToPictureList: ${val}`);
   const newListItem = document.createElement('li');
   const newListItemContent = document.createTextNode(val);
   newListItem.appendChild(newListItemContent);
   pictureList.append(newListItem);
 }
 
-function sumOfPictureWidths(arr) {
+function sumOfArray(arr) {
   const initialValue = 0;
   return arr.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
@@ -40,8 +34,43 @@ function displayTotalWidthOfPictures(sum) {
   totalPictures.textContent = `total width: ${sum}`;
 }
 
-const form = document.getElementById('form');
+function wallFormSubmit(event) {
+  wallHandler(wallForm.enter_wall.value);
+  wallForm.reset();
+  event.preventDefault();
+}
+
+function wallHandler(val) {
+  displayWallWidth(Number.parseInt(val));
+}
+
+function displayWallWidth(val) {
+  totalWall.textContent = `width: ${val}`;
+}
+
+function insetFormSubmit(event) {
+  insetHandler(insetForm.enter_inset.value);
+  insetForm.reset();
+  event.preventDefault();
+}
+
+function insetHandler(val) {
+  let total = val * 2;
+  displayInsetWidth(Number.parseInt(total));
+}
+
+function displayInsetWidth(val) {
+  totalInset.textContent = `inset: ${val}`;
+}
+
+const pictureForm = document.getElementById('picture-form');
+const wallForm = document.getElementById('wall-form');
+const insetForm = document.getElementById('inset-form');
 const pictureList = document.getElementById('picture-list');
 const totalPictures = document.getElementById('total-pictures');
 const totalWall = document.getElementById('total-wall');
-form.addEventListener('submit', formSubmit);
+const totalInset = document.getElementById('total-inset');
+
+pictureForm.addEventListener('submit', pictureFormSubmit);
+wallForm.addEventListener('submit', wallFormSubmit);
+insetForm.addEventListener('submit', insetFormSubmit);
